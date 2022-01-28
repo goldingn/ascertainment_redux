4e62c0ef85b16c62dce9cb9da6dcab91a1bb8f4d
Nick Golding <goldingn35@gmail.com>
2022-01-28 12:11:18 +0800

    first pass at fitting greta model

--- analysis.R
+++ analysis.R
@@ -6,6 +6,9 @@ set.seed(2)
 # simulate fake parameters
 data <- sim_parameters()
 
+# simulate fake reason_for_test counts over time, with these parameters
+reason_test_count_data <- sim_reason_for_test_data(data, sample_size = 200, days_between_surveys = 7)
+
 # extract required parameters
 params <- data %>%
   pivot_wider(
@@ -13,8 +16,16 @@ params <- data %>%
     values_from = value
   )
 
+# define the contact fraction as an unknown variable - logit-linear model on date
+date_num <- as.numeric(params$date - min(params$date))
+contact_fraction_intercept <- normal(0, 1)
+contact_fraction_slope <- normal(0, 1)
+logit_contact_fraction <- contact_fraction_intercept + contact_fraction_slope * date_num
+contact_fraction <- ilogit(logit_contact_fraction)
+
+
 expected_fractions <- calculate_expected_fractions(
-  contact_fraction = params$contact_fraction,
+  contact_fraction = contact_fraction,
   contact_test_prob = params$contact_test_prob,
   symptomatic_fraction = params$symptomatic_fraction,
   symptomatic_test_prob = params$symptomatic_test_prob,
@@ -24,6 +35,34 @@ expected_fractions <- calculate_expected_fractions(
 
 ascertainment <- expected_fractions$ascertainment
 reason_test_fraction <- expected_fractions$reason_test_fraction
+
+# pull out the modelled reason for test fractions corresponding to the data
+idx <- match(reason_test_count_data$date, params$date)
+observed_reason_counts <- reason_test_count_data %>%
+  select(
+    -date
+  ) %>%
+  as_data()
+
+distribution(observed_reason_counts) <- multinomial(
+  size = rowSums(observed_reason_counts),
+  prob = reason_test_fraction[idx, ]
+)
+
+
+# define and fit model
+m <- model(contact_fraction_intercept, contact_fraction_slope)
+draws <- mcmc(m)
+
+
+plot(draws)
+
+
+# compute posterior means and CIs of ascertainment and key parameter
+
+# use only data dates when fitting, then predict post-hoc
+
+
 
 # get output from greta model to confirm it matches
 
